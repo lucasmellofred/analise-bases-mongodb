8babe0d6adae45aa61206b5216ea5b5b01eb8cc0
Lucas Frederico Roeder de Mello <104468335+lucasmellofred@users.noreply.github.com>
2024-06-12 16:02:37 -0300

    Create tiposDados.js

---
+++ dados atmosféricos/tiposDados.js
@@ -0,0 +1,100 @@
+db.data.aggregate([
+  {
+    $project: {
+      fieldNamesAndTypes: {
+        $map: {
+          input: { $objectToArray: "$$ROOT" },
+          as: "field",
+          in: {
+            k: "$$field.k",
+            v: { $type: "$$field.v" }
+          }
+        }
+      },
+      fieldCount: { $size: { $objectToArray: "$$ROOT" } }
+    }
+  },
+  {
+    $facet: {
+      fieldTypes: [
+        {
+          $unwind: "$fieldNamesAndTypes"
+        },
+        {
+          $group: {
+            _id: "$fieldNamesAndTypes.k",
+            types: { $addToSet: "$fieldNamesAndTypes.v" }
+          }
+        },
+        {
+          $group: {
+            _id: null,
+            fieldTypes: {
+              $push: {
+                k: "$_id",
+                v: { $arrayElemAt: ["$types", 0] }
+              }
+            }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            fieldTypes: {
+              $arrayToObject: "$fieldTypes"
+            }
+          }
+        }
+      ],
+      fieldCounts: [
+        {
+          $group: {
+            _id: null,
+            averageFieldCount: { $avg: "$fieldCount" },
+            maxFieldCount: { $max: "$fieldCount" },
+            minFieldCount: { $min: "$fieldCount" }
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            averageFieldCount: 1,
+            maxFieldCount: 1,
+            minFieldCount: 1
+          }
+        }
+      ]
+    }
+  },
+  {
+    $project: {
+      fieldTypes: { $arrayElemAt: ["$fieldTypes.fieldTypes", 0] },
+      fieldCounts: { $arrayElemAt: ["$fieldCounts", 0] }
+    }
+  },
+  {
+    $project: {
+      fieldTypes: {
+        $objectToArray: "$fieldTypes"
+      },
+      fieldCounts: 1
+    }
+  },
+  {
+    $unwind: "$fieldTypes"
+  },
+  {
+    $sort: {
+      "fieldTypes.k": 1
+    }
+  },
+  {
+    $group: {
+      _id: null,
+      fieldTypes: { $push: "$fieldTypes" },
+      fieldCounts: { $first: "$fieldCounts" }
+    }
+  }
+]).forEach(function(doc) {
+  printjson(doc);
+});
